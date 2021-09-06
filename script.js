f11325defc03d3389fbfb3deed3a8ae950f45160
Faisal <md.faisal1899@yahoo.com>
2021-09-06 09:11:19 +0600

    delete and style, class, attribute

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-const haeder = document.querySelector('.header');
+const header = document.querySelector('.header');
 
 const allSections = document.querySelectorAll('.section');
 console.log(allSections); //Nodelist
@@ -46,3 +46,63 @@ const allButtons = document.getElementsByClassName('btn');
 console.log(allButtons); //Html Collection
 
 document.getElementsByTagName('button');
+
+//An html collection is updated in real time  . But, Nodelist is depend on when we create the nodelist, then it will not be updated. It will be the same .
+
+//create element
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies to improve our user experience. <button class = "btn btn--close-cookie"> Got it ! </button>';
+
+// header.prepend(message);
+// header.append(message.cloneNode(true));
+header.append(message);
+console.log(message);
+
+//before after... what is the difference between before/prepend and after/append
+
+// delete
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove(); // latest addition
+    message.parentElement.removeChild(message); // old method
+  });
+
+//style.
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color); // can't console it. because it's an inline element
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).height);
+console.log(getComputedStyle(message).color);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 0 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+// absolute value pyte hoile getAttribute use kora lagbe.
+
+logo.setAttribute('company', 'Bankist'); // company naame a ekta attribut setted, jar value bankist
+
+// data attribute // special
+
+console.log(logo.dataset.versionNumber);
+
+// logo
+//.classList.add,remove,toggle,contains
+
+//logo.className = 'faisal'// overwrite all classname, and add only one classname
